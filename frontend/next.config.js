25adb1af81a53a624530222e8769e4a13afcfda3
Akshat Agrawal <agrawalakshat234@gmail.com>
2026-01-27 00:46:39 +0530

    Fix Next.js App Router build issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,27 +1,6 @@
-// module.exports = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'i5.walmartimages.com',
-//         port: '',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'firebasestorage.googleapis.com',
-//         port: '',
-//       },
-//     ],
-//   },
-// }
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
